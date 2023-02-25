4259590e46328c22475145c20bd1002e2405a34d
allegrasantoro <allegrasantoro.cs@gmail.com>
2023-02-25 16:33:39 +0000

    added player movement

---
+++ super-tetris-world/src/scripts/Global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var game_paused = false
+var has_lost = false
+var tile_size = 64
---
+++ super-tetris-world/src/scripts/Player.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+var gravity
+var max_jump_velocity
+var min_jump_velocity
+
+var speed = 5 * Global.tile_size
+var max_jump_height = 2.25 * Global.tile_size
+var min_jump_height = 0.8 * Global.tile_size
+var jump_duration = 0.5
+
+var velocity = Vector2.ZERO
+
+func _ready():
+	gravity = 2 * max_jump_height / pow(jump_duration, 2)
+	max_jump_velocity = -sqrt(2 * gravity * max_jump_height)
+	min_jump_velocity = -sqrt(2 * gravity * min_jump_height)
+
+func _physics_process(delta):
+	check_if_key_pressed(delta)
+	velocity.y += gravity * delta
+	move_and_slide(velocity, Vector2.UP)
+
+func check_if_key_pressed(delta):
+	if not Global.has_lost and not Global.game_paused:
+		move(delta)
+
+func move(delta):
+	velocity.x = 0
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = max_jump_velocity
+	if Input.is_action_just_released("jump") and velocity.y < min_jump_velocity:
+		velocity.y = min_jump_velocity
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+
+
