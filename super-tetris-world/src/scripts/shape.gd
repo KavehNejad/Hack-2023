d780b0d2f535c3a1bd2f974ea3d928f9e56a859c
Kaveh Nejad <kaveh2009@hotmail.co.uk>
2023-02-25 16:57:47 +0000

    blocks can collide down

--- super-tetris-world/src/scripts/shape.gd
+++ super-tetris-world/src/scripts/shape.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var block_scene = preload("res://src/scenes/block.tscn")
+var blocks = []
+var current = true
 
 func set_info(shape_info):
 	for y in range(len(shape_info['layout'])):
@@ -10,13 +12,46 @@ func set_info(shape_info):
 				block.position.x = 64 * x
 				block.position.y = 64 * y
 				add_child(block)
+				blocks.append(block)
 
 func _on_Timer_timeout():
-	position.y += 64
-	
+	remote_from_grid()
+	if would_overlap('down'):
+		current = false
+	else:
+		position.y += 64
+	add_to_grid()
+
+func _ready():
+	add_to_grid()
+
 func _physics_process(delta):
+	if !current:
+		return
 	#checks for stuff every frame
 	if Input.is_action_just_pressed("block_right"):
+		remote_from_grid()
 		position.x += 64
+		add_to_grid()
 	if Input.is_action_just_pressed("block_left"):
+		remote_from_grid()
 		position.x -= 64
+		add_to_grid()
+
+func remote_from_grid():
+	for block in blocks:
+		get_parent().remote_block(position.x + block.position.x, position.y + block.position.y)
+
+func add_to_grid():
+	for block in blocks:
+		get_parent().add_block(position.x + block.position.x, position.y + block.position.y)
+
+func would_overlap(direction):
+	var pos = position
+	if direction == 'down':
+		pos.y += 64
+	for block in blocks:
+		var indexs = get_parent().get_block_index(pos.x + block.position.x, pos.y + block.position.y)
+		if get_parent().get_block_by_index(indexs['x'], indexs['y']) == 1:
+			return true
+	return false
--- super-tetris-world/src/scripts/world.gd
+++ super-tetris-world/src/scripts/world.gd
@@ -2,8 +2,31 @@ extends Node
 
 var set_up_blocks_script = load("res://src/scripts/set_up_block_types.gd").new()
 
+var grid = []
+
 func _ready():
 	var shapes = set_up_blocks_script.get_block_tyoes()
+	create_grid()
+	shapes[1].position.x = 64 * 1
+	shapes[1].position.y = 64 * 4
 	add_child(shapes[1])
-	shapes[1].position.x = 200
-	shapes[1].position.y = 200
+
+func create_grid():
+	for y in range(100):
+		grid.append([])
+		for x in range(100):
+			grid[y].append(0)
+
+func get_block_index(x, y):
+	return { 'x': round(x / 64), 'y': round(y / 64) }
+
+func get_block_by_index(x, y):
+	return grid[y][x]
+
+func add_block(x, y):
+	var indexs = get_block_index(x, y)
+	grid[indexs['y']][indexs['x']] = 1
+
+func remote_block(x, y):
+	var indexs = get_block_index(x, y)
+	grid[indexs['y']][indexs['x']] = 0
