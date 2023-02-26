3d9b81a845e5665940870cbef43d20a1210856ae
allegrasantoro <allegrasantoro.cs@gmail.com>
2023-02-26 11:31:01 +0000

    flag and collision shape

--- super-tetris-world/src/scripts/Checkpoint.gd
+++ super-tetris-world/src/scripts/Checkpoint.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
+func _ready():
+	$AnimatedSprite.play("still-pole")
+
 func _process(delta):
 	for body in $Area2D.get_overlapping_bodies():
 		if body.is_in_group('Player'):
 			print("IN")
+			$AnimatedSprite.play("pole-going-up")
 			get_parent().create_a_checkpoint()
 	
