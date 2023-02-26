93bfb3e413523c7b0f39ef6071a83dc5009d0d04
Kaveh Nejad <kaveh2009@hotmail.co.uk>
2023-02-26 10:03:32 +0000

    Update checkpoint

--- super-tetris-world/src/scripts/Checkpoint.gd
+++ super-tetris-world/src/scripts/Checkpoint.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 func _process(delta):
-	for body in get_overlapping_bodies():
+	for body in $Area2D.get_overlapping_bodies():
 		if body.is_in_group('Player'):
+			print("IN")
 			get_parent().create_a_checkpoint()
 	
