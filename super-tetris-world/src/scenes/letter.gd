de58c0cee167bf39b60eacbc9521eace9290404f
allegrasantoro <allegrasantoro.cs@gmail.com>
2023-02-27 08:23:09 +0000

    added initial scene

---
+++ super-tetris-world/src/scenes/letter.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var goodbye = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("fade-in")
+
+func _process(delta):
+	if Input.is_action_pressed("skip"):
+		$AnimationPlayer.play("fade-out")
+		goodbye = true 
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if (goodbye):
+		get_tree().change_scene("res://src/scenes/Tutorial level.tscn")
